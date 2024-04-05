d1e1004fbef8b091434736d8cb01dfbaf73c1ad7
Cbragg-code <braggc1@etsu.edu>
2024-04-05 15:00:47 -0400

    Last update
    fixed comments

--- BucStop/Tests/TestSetup.cs
+++ BucStop/Tests/TestSetup.cs
@@ -7,18 +7,22 @@ namespace BucStop.Tests
     public class TestSetup
     {
         protected IWebDriver driver;
-        protected readonly string baseUrl = "https://localhost:7182/"; // Or externalize this configuration
+        protected readonly string baseUrl = "https://localhost:7182/";
 
+        /// <summary>
+        /// Immediately before each test initialize the driver and options.
+        /// </summary>
         [SetUp]
         public virtual void SetUp()
         {
             var options = new ChromeOptions();
-            options.AddArgument("--headless");
-            options.AddArgument("--ignore-certificate-errors");
+            options.AddArgument("--headless"); // Make Chrome run without a GUI
             driver = new ChromeDriver(options);
             driver.Navigate().GoToUrl(baseUrl);
         }
-
+        /// <summary>
+        /// Immediately after each test, close the driver.
+        /// </summary>
         [TearDown]
         public virtual void TearDown()
         {
